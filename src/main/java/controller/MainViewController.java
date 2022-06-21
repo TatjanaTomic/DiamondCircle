66ef5c558734fb20f2d44b9426df75a7d8f00090
TatjanaTomic <tatjanatomic997@yahoo.com>
2022-06-21 20:51:50 +0200

    Finished showing players' labels

--- src/main/java/controller/MainViewController.java
+++ src/main/java/controller/MainViewController.java
@@ -6,18 +6,22 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import model.exception.ErrorStartingGameException;
 import model.field.Coordinates;
 import model.field.EmptyField;
 import model.field.Field;
 import model.field.GameField;
 import model.game.Game;
+import model.game.Simulation;
+import model.util.Util;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainViewController implements Initializable {
 
-    private static final String numberOfGamesText = "Trenutni broj odigranih igara: ";
+    private final String numberOfGamesText = "Trenutni broj odigranih igara: ";
 
     private final double mapWidth = 500;
     private final double mapHeight = 500;
@@ -31,8 +35,18 @@ public class MainViewController implements Initializable {
     private final Color startFieldColor = Color.WHITESMOKE;
     private final Color endFieldColor = Color.rgb(100,15,115);
 
+    private static Simulation simulation = null;
+
     @FXML
     public Label numberOfGamesLabel;
+    @FXML
+    public Label player1Label;
+    @FXML
+    public Label player2Label;
+    @FXML
+    public Label player3Label;
+    @FXML
+    public Label player4Label;
 
     @FXML
     public Button startStopButton;
@@ -40,13 +54,16 @@ public class MainViewController implements Initializable {
     @FXML
     public GridPane mapGridPane;
 
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        if(simulation == null)
+            return;
+
         numberOfGamesLabel.setText(numberOfGamesText + Game.numberOfGames);
 
+        initializePlayersLabels();
         initializeMap();
-
 
     }
 
@@ -58,7 +75,7 @@ public class MainViewController implements Initializable {
 
         for(int i = 1; i < Game.gamePath.size(); i++) {
             Coordinates coordinates = Game.gamePath.get(i);
-            Field field = new GameField("T", coordinates, fieldWidth, fieldHeight, Color.rgb(redComponent, greenComponent, blueComponent));
+            Field field = new GameField("Test", coordinates, fieldWidth, fieldHeight, Color.rgb(redComponent, greenComponent, blueComponent));
             mapGridPane.add(field, coordinates.getY(), coordinates.getX());
             redComponent -= 3;
             greenComponent -= 5;
@@ -77,5 +94,17 @@ public class MainViewController implements Initializable {
 
     }
 
+    private void initializePlayersLabels() {
 
+        Label[] playersLabels = {player1Label, player2Label, player3Label, player4Label};
+
+        for(int i = 1; i <= Game.numberOfPlayers; i++) {
+            playersLabels[i-1].setText("Igrač " + i +": " + simulation.getPlayers().get(i-1).getName());
+            playersLabels[i-1].setTextFill(Paint.valueOf(simulation.getPlayers().get(i-1).getColor().toString()));
+        }
+    }
+
+    public static void setSimulation(Simulation sim) {
+        simulation = sim;
+    }
 }
